276a29450dcc005115915c33b4f726fe1bbc1a61
yuraup <o721@naver.com>
2021-07-22 17:37:00 +0900

    회원가입 jsx, css (수정 전)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Router from './components/Router';
+import Join from './routes/Join/Join';
 
 function App() {
   return (
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from "../routes/Auth/Login";
+import Join from "../routes/Join/Join";
 
 export default () => (
   <Router>
     {/* 임의로 첫 화면 로그인 화면으로 설정 */}
     <Route exact path="/" component={Login} />
+    <Route exact path="/Join" component={Join} />
   </Router>
 );
