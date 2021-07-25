763674d58e874640e90c916fd8f2dea712eea79a
qkrthdus605 <qkrthdus605@gmail.com>
2021-07-25 12:31:29 +0900

    라우터 경로 설정 (회원가입 페이지 경로 추가 필요)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 
+// 라우팅
 function App() {
   return (
-    <LoginPage />
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} /> 
+          <Route exact path="/login" component={LoginPage} /> 
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
