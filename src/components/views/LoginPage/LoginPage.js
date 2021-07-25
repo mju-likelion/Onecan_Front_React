4bab5c868437de8b15f50884c71bbba102c38601
qkrthdus605 <qkrthdus605@gmail.com>
2021-07-25 12:32:29 +0900

    로그인 페이지 구현 (css 추가해야함)

--- src/components/views/LoginPage/LoginPage.js
+++ src/components/views/LoginPage/LoginPage.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
 
-function LoginPage() {
+function LoginPage(props) {
+
+  const dispatch = useDispatch();
+
+  const [Email, setEmail] = useState("")
+  const [Password, setPassword] = useState("")
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value)
+  }
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value)
+  }
+
+  const onSubmitHandler =(event) => {
+    // 새로고침 안되도록
+    event.preventDefault();  
+    let body = {
+      email : Email,
+      password : Password
+    }
+    
+    // 백엔드 구현이 안되서 실행이 되지는 않음 
+    dispatch(loginUser(body))
+        .then(response => {
+          if(response.payload.loginSuccess){
+            props.history.push('/')
+          }else{
+            alert('아이디와 비밀번호가 일치하지 않습니다.')
+          }
+        })
+    
+  }
+
   return (
-    <div>
-      LoginPage
+    <div style={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center',
+      width: '100%', height: '100vh'
+    }}>
+      <form style={{ display: 'flex', flexDirection: 'column' }}
+          onSubmit={onSubmitHandler}>
+        <label>ID</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>PW</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <br />
+        <button type="submit">
+          Login
+        </button>
+      </form>
     </div>
   )
 }
 
-export default LoginPage
+export default LoginPage;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App />
+  </Provider>
+  , document.getElementById('root')
 );
 
