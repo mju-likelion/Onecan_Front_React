aa752f878398c96b2cd23da01e48b0023deda1a4
김 소 현 <sooooooohyeon5@naver.com>
2021-07-25 17:44:29 +0900

    회원가입 디자인

--- src/components/views/Header/Header.js
+++ src/components/views/Header/Header.js
@@ -1,52 +1,50 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
-function Header(){
+function Header() {
   const onClickLogin = (e) => {
     // 로그인 페이지로 이동
-    document.location.href = "/" 
+    document.location.href = "/login";
   };
 
   const onClickJoin = (e) => {
-    document.location.href = "/join"
+    document.location.href = "/register";
   };
-  
+
   const onClickFQ = (e) => {
-    document.location.href = "/f&q"
-  }
-  return(
+    document.location.href = "/f&q";
+  };
+  return (
     <>
       {/* 헤더 */}
       <header className="HeaderContainer">
-      <div className="auth">
-        <ul>
-          <li onClick={onClickLogin}>로그인</li>
-          <li onClick={onClickJoin}>회원가입</li>
-          <li onClick={onClickFQ}>F&Q</li>
-        </ul>
-      </div>
-
-      <div className="title_search">
-        <div className="title">
-          One can do it!
-        </div>
-        <div className="search">
-          <input placeholder="검색어를 입력하세요"></input> 
-        </div>
-      </div>
-      <div className="category">
-        <div className="category_list">
+        <div className="auth">
           <ul>
-            <li>전체 카테고리</li>
-            <li>Best</li>
-            <li>Sale</li>
-            <li>커뮤니티</li>
-            <li>장바구니</li>
+            <li onClick={onClickLogin}>로그인</li>
+            <li onClick={onClickJoin}>회원가입</li>
+            <li onClick={onClickFQ}>F&Q</li>
           </ul>
         </div>
-      </div>
-    </header>
-  </>
+
+        <div className="title_search">
+          <div className="title">One can do it!</div>
+          <div className="search">
+            <input placeholder="검색어를 입력하세요"></input>
+          </div>
+        </div>
+        <div className="category">
+          <div className="category_list">
+            <ul>
+              <li>전체 카테고리</li>
+              <li>Best</li>
+              <li>Sale</li>
+              <li>커뮤니티</li>
+              <li>장바구니</li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
 
--- src/components/views/RegisterPage/RegisterPage.js
+++ src/components/views/RegisterPage/RegisterPage.js
@@ -1,128 +1,167 @@
-import React, {useState} from 'react';
-import {useDispatch} from 'react-redux';
-import {registerUser} from '../../../_actions/user_action';
-import Axios from 'axios';
-import {withRouter} from 'react-router-dom';
-import Header from '../../../components/views/Header/Header';
-import Footer from '../../../components/views/Footer/Footer';
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_actions/user_action";
+import { withRouter } from "react-router-dom";
+import Header from "../../../components/views/Header/Header";
+import Footer from "../../../components/views/Footer/Footer";
+
+import { Form, Input, Button } from "antd";
 
 function RegisterPage(props) {
-  const dispatch = useDispatch ();
+  const dispatch = useDispatch();
 
-  const [Email, setEmail] = useState("")
-  const [Name, setName] = useState("")
-  const [Password, setPassword] = useState("")
-  const [ConfirmPassword, setConfirmPassword] = useState("")
-  const [Address, setAddress] = useState("")
-  const [Phone, setPhone] = useState("")
+  const [Email, setEmail] = useState("");
+  const [Name, setName] = useState("");
+  const [Password, setPassword] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+  const [Address, setAddress] = useState("");
+  const [Phone, setPhone] = useState("");
 
   const onEmailHandler = (event) => {
-    setEmail(event.currentTarget.Value)
-  }
+    setEmail(event.currentTarget.Value);
+  };
 
   const onNameHandler = (event) => {
-    setName(event.currentTarget.Value)
-  }
+    setName(event.currentTarget.Value);
+  };
 
   const onPasswordHandler = (event) => {
-    setPassword(event.currentTarget.Value)
-  }
+    setPassword(event.currentTarget.Value);
+  };
 
   const onConfirmPasswordHandler = (event) => {
-    setConfirmPassword(event.currentTarget.Value)
-  }
+    setConfirmPassword(event.currentTarget.Value);
+  };
 
   const onAddressHandler = (event) => {
-    setAddress(event.currentTarget.Value)
-  }
+    setAddress(event.currentTarget.Value);
+  };
 
   const onPhoneHandler = (e) => {
     const regex = /^[0-9\b -]{0,13}$/;
     if (regex.test(e.target.value)) {
       setPhone(e.target.value);
     }
-  } //phone number 하이픈 넣는 거 진행 중
+  }; //phone
 
   const onSubmitHandler = (event) => {
-      event.preventDefault();
+    event.preventDefault();
+    console.log("you clicked submit!");
 
-      if(Password !== ConfirmPassword) {
-          return alert('비밀번호와 비밀번호 확인은 같아야 합니다.')
-      }
-
-      let body = {
-        email: Email,
-        password: Password,
-        name: Name
-
-      }
-
-      dispatch(registerUser(body))
-            .then(response => {
-              if(response.payload.success) {
-                  props.history.push("/login")
-              } else {
-                    alert("회원가입에 실패했습니다.")
-              }
-            })
+    if (Password !== ConfirmPassword) {
+      return alert("비밀번호와 비밀번호 확인은 같아야 합니다.");
     }
 
+    let body = {
+      email: Email,
+      password: Password,
+      name: Name,
+    };
 
-    return (
+    dispatch(registerUser(body)).then((response) => {
+      if (response.payload.success) {
+        props.history.push("/login");
+      } else {
+        alert("회원가입에 실패했습니다.");
+      }
+    });
+  };
 
-      <>
+  useEffect(() => {
+    if (Phone.length === 10) {
+      setPhone(Phone.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3"));
+    }
+    if (Phone.length === 13) {
+      setPhone(
+        Phone.replace(/-/g, "").replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3")
+      );
+    }
+  }, [Phone]); //phone 하이픈 넣음
+
+  return (
+    <>
       <Header />
 
-           <div style= {{
-        display: 'flex', justifyContent: 'center', alignItems: 'center',
-        width: '100%', height: '100vh'
-      }}
-
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100vh",
+        }}
       >
-      <div style = {{paddingTop: 250}}>
-          <br/>
-          <h1 style = {{paddingTop:20}}>One can do it!</h1>
-          <h2 style = {{display:'flex', justifyContent:'center'}}>회원 가입</h2>
+        <div style={{ paddingTop: 250 }}>
           <br />
-          <form style={{display: 'flex', flexDirection:'column'}}
-                onSubmit={onSubmitHandler}>
+          <h1 style={{ paddingTop: 20 }}>One can do it!</h1>
+          <h2 style={{ display: "flex", justifyContent: "center" }}>
+            회원 가입
+          </h2>
+          <br />
+          <form
+            style={{ display: "flex", flexDirection: "column" }}
+            onSubmit={onSubmitHandler}
+          >
+            <label>이메일(아이디)</label>
+            <Input
+              id="email"
+              placeholder="이메일을 입력하세요"
+              type="text"
+              value={Email}
+              onChange={onEmailHandler}
+            />
 
-                <label>이메일(아이디)</label>
-                <input type="email" value={Email} onChange={onEmailHandler} />
-                
-                <label>이름</label>
-                <input type="text" value={Name} onChange={onNameHandler} />
+            <label>이름</label>
+            <Input
+              id="name"
+              placeholder="이름을 입력하세요"
+              type="text"
+              value={Name}
+              onChange={onNameHandler}
+            />
 
-                <label>비밀번호</label>
-                <input type="password" value={Password} onChange={onPasswordHandler} />
+            <label>비밀번호</label>
+            <input
+              type="password"
+              value={Password}
+              onChange={onPasswordHandler}
+            />
 
-                <label>비밀번호 확인</label>
-                <input type="password" value={ConfirmPassword} onChange={onConfirmPasswordHandler} />
-                <br />
-                
-                <label>주소</label>
-                <input type="text" value={Address} onChange={onAddressHandler} />
+            <label>비밀번호 확인</label>
+            <input
+              type="password"
+              value={ConfirmPassword}
+              onChange={onConfirmPasswordHandler}
+            />
+            <br />
 
-                <label>전화 번호(phone)</label>
-                <input type="text" value={Phone} onChange={onPhoneHandler} />
-                <br />
-              
-                <label>
-                개인정보 수집 및 활용 동의
-                <input type="checkbox" /></label>
-                <br />
+            <label>주소</label>
+            <input type="text" value={Address} onChange={onAddressHandler} />
 
-                <button type="submit" style= {{color:'green'}}>
-                  회원 가입
-                </button>
-                
-            </form>
+            <label>전화 번호(콜)</label>
+            <input type="text" value={Phone} onChange={onPhoneHandler} />
+            <br />
+
+            <label>
+              개인정보 수집 및 활용 동의
+              <input type="checkbox" />
+            </label>
+            <br />
+
+            <Button
+              type="submit"
+              onClick={onSubmitHandler}
+              style={{ color: "green" }}
+            >
+              회원 가입
+            </Button>
+          </form>
         </div>
       </div>
 
       <Footer />
     </>
-    )
+  );
 }
 
 export default withRouter(RegisterPage);
