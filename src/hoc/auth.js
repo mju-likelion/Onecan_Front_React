ba1b4720c16a02295dc8e447724bdbefbd5a4c9f
김 소 현 <sooooooohyeon5@naver.com>
2021-07-25 21:05:15 +0900

    가로 스크롤바 제거

--- src/hoc/auth.js
+++ src/hoc/auth.js
@@ -1,48 +1,38 @@
-import React, { useEffect } from 'react';
-import Axios from 'axios';
-import {useDispatch} from 'react-redux';
-import {auth} from '../_actions/user_action';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { auth } from "../_actions/user_action";
 
 export default function (SpecificComponent, option, adminRoute = null) {
+  //<option>
+  //null => 아무나 출입 가능한 페이지
+  //true => 로그인한 유저만 출입이 가능한 페이지
+  //false => 로그인한 유저는 출입 불가능한 페이지
 
-        //<option>
-        //null => 아무나 출입 가능한 페이지
-        //true => 로그인한 유저만 출입이 가능한 페이지
-        //false => 로그인한 유저는 출입 불가능한 페이지
+  function AuthenticationCheck(props) {
+    const dispatch = useDispatch();
 
-        function AuthenticationCheck(props) {
-            const dispatch = useDispatch();
+    useEffect(() => {
+      dispatch(auth()).then((response) => {
+        console.log(response);
 
-            useEffect(() => {
+        //로그인 하지 않은 상태
+        if (!response.payload.isAuth) {
+          if (option) {
+            props.history.push("/login");
+          }
+        } else {
+          //로그인 한 상태
+          if (adminRoute && !response.payload.isAdmin) {
+            props.history.push("/");
+          } else {
+            if (option === false) props.history.push("/");
+          }
+        }
+      });
+    }, []);
 
-              dispatch(auth()).then(response => {
-                console.log(response)
-                
-                //로그인 하지 않은 상태
-                if(!response.payload.isAuth) {
-                    if (option) {
-                        props.history.push('/login')
-                    }
+    return <SpecificComponent />;
+  }
 
-                } else {
-                    //로그인 한 상태
-                    if (adminRoute && !response.payload.isAdmin) {
-                        props.history.push('/')
-                    } else {
-                          if(option === false)
-                          props.history.push('/')
-                    }
-
-                }
-              })
-
-            }, [])
-
-            return (
-                  <SpecificComponent />
-            )
-
-        };
-
-  return AuthenticationCheck
-};
+  return AuthenticationCheck;
+}
