758288de3754aa9e8160611a4ae9987f60750338
qkrthdus605 <qkrthdus605@gmail.com>
2021-07-26 15:51:53 +0900

    LoginPage css 수정

--- src/components/views/LoginPage/LoginPage.js
+++ src/components/views/LoginPage/LoginPage.js
@@ -48,25 +48,84 @@ function LoginPage(props) {
       display: 'flex', justifyContent: 'center', alignItems: 'center',
       width: '100%', height: '100vh'
     }}>
-      <form style={{ display: 'flex', flexDirection: 'column' }}
+      <form style={{ display: 'flex', flexDirection: 'column', marginLeft: 480, marginRight: 480}}
           onSubmit={onSubmitHandler}>
-        <label>ID</label>
-        <input type="email" value={Email} onChange={onEmailHandler} />
-        <label>PW</label>
-        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <p style={{ fontSize: 40, fontFamily: 'Montserrat', textAlign: 'center'}}>Login</p>
+        <label style={{ fontSize: 20, fontFamily: 'Montserrat'}}>ID</label>
+        <input 
+           type="email" 
+           value={Email} 
+           onChange={onEmailHandler} 
+           style={{
+             width: 300, 
+             height: 30, 
+             borderRadius: 9, 
+             color: '#E0E0E0'}} />
+        <label style={{
+           fontSize: 20, 
+           fontFamily: 'Montserrat'}}>PW</label>
+        <input 
+          type="password" 
+          value={Password} 
+          onChange={onPasswordHandler} 
+          style={{
+            width: 300, 
+            height: 30, 
+            borderRadius: 9, 
+            color: '#E0E0E0'}} />
         <br />
-        <button type="submit">
+        <button 
+          type="submit"
+          style={{
+            height: 25,
+            backgroundColor: '#31572E',
+            color: 'white',
+            borderRadius: 9,
+            marginTop: 10,
+            border: 0
+          }}>
           로그인
         </button>
-        <button>
-          카카오로 로그인하기
+        <button
+          style={{
+            height: 25,
+            backgroundColor: 'yellow',
+            color: 'black',
+            borderRadius: 9,
+            marginTop: 20,
+            border: 0
+          }}>
+          Kakao
         </button>
-        <button>
-          네이버로 로그인하기
+        <button
+          style={{
+            height: 25,
+            backgroundColor: '#15CB60',
+            color: 'black',
+            borderRadius: 9,
+            marginTop: 10,
+            border: 0
+          }}>
+          Naver
         </button>
-        <button>
-          구글로 로그인하기
+        <button
+          style={{
+            height: 25,
+            backgroundColor: 'white',
+            color: 'black',
+            borderRadius: 9,
+            marginTop: 10,
+            borderBlockStyle: 'solid',
+            borderColor: 'blue'
+          }}>
+          Google
         </button>
+        <p style={{
+          fontFamily: 'Montserrat',
+          fontSize: 15,
+          textAlign: 'center',
+          marginTop: 20
+        }}>회원가입 | 아이디 비밀번호 찾기</p>
       </form>
     </div>
     <Footer />
