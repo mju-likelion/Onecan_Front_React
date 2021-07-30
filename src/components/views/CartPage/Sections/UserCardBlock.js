d5f1638fd28bc567b038b93bb51b6ee4bfe0126f
qkrthdus605 <qkrthdus605@gmail.com>
2021-07-30 20:34:19 +0900

    장바구니 상품 목록 카드 블록 js 및 css 파일 생성

---
+++ src/components/views/CartPage/Sections/UserCardBlock.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'UserCardBlock.css';
+
+function UserCardBlock(props){
+  return(
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>상품</th>
+            <th>수량</th>
+            <th>상품 가격</th>
+            <th>상품 삭제</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+        </tbody>
+
+      </table>
+    </div>
+  )
+}
+
+export default UserCardBlock;
