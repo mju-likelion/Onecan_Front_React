775ec187d445215135ac07e244497a1792230bb8
qkrthdus605 <qkrthdus605@gmail.com>
2021-08-04 14:36:21 +0900

    헤더 로그인/로그아웃 버튼 합치기 완료(추후 테스트 필요)

--- src/components/views/MainPage/MainPage.js
+++ src/components/views/MainPage/MainPage.js
@@ -11,16 +11,6 @@ function MainPage(props) {
           .then(response => { console.log(response) })
     }, [])
 
-    const onClickHandler = () => {
-      axios.get('/api/users/logout')
-          .then(response => {
-              if (response.data.success) {
-                  props.history.push("/login")
-              } else {
-                alert('로그아웃에 실패하였습니다.')
-              }
-          })
-    }
 
     return (
       <>
@@ -30,10 +20,6 @@ function MainPage(props) {
         width: '100%', height: '100vh'
       }}>
         <h2>시작 페이지</h2>
-
-        <button onClick={onClickHandler}>
-          로그아웃
-        </button>
       </div>
       <Footer />
       </>
