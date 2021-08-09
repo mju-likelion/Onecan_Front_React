4b5738e6b03b43ebb6d328698b0591c414947888
qkrthdus605 <qkrthdus605@gmail.com>
2021-08-09 16:19:54 +0900

    이미지 슬라이더 구현

--- src/components/views/MainPage/MainPage.js
+++ src/components/views/MainPage/MainPage.js
@@ -3,27 +3,15 @@ import React, {useEffect} from 'react';
 import {withRouter} from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import ImageGallery from 'react-image-gallery'
+import { Carousel } from 'antd';
 import './MainPage.css';
 
 function MainPage(props) {
 
-    
-    const images = [
-      {
-        original: 'https://picsum.photos/id/1018/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1018/250/150/',
-      },
-      {
-        original: 'https://picsum.photos/id/1015/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1015/250/150/',
-      },
-      {
-        original: 'https://picsum.photos/id/1019/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1019/250/150/',
-      },
-    ];
-
+    const imgStyle = {
+      width: '100%',
+      height: '280px',
+    };
 
     useEffect(() => {
       axios.get('/api/hello')
@@ -35,37 +23,58 @@ function MainPage(props) {
       <>
       <Header />
       <div style={{
-        marginTop: '300px',
+        marginTop: '200px',
         width: '100%',
         height: '120px'
       }}>
-        {/* 이미지 슬라이더 필요 */}
-        <ImageGallery items={images} />
+        <Carousel autoplay>
+          <div>
+            <img style={imgStyle}
+              src="https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466_1280.jpg" 
+              alt="음식" />
+          </div>
+          <div>
+            <img style={imgStyle}
+              src="https://cdn.pixabay.com/photo/2017/08/30/17/12/waffle-hearts-2697904_1280.jpg" 
+              alt="음식" />
+          </div>
+          <div>
+            <img style={imgStyle}
+              src="https://cdn.pixabay.com/photo/2016/11/06/23/31/breakfast-1804457_1280.jpg" 
+              alt="음식" />
+          </div>
+          <div>
+            <img style={imgStyle}
+              src="https://cdn.pixabay.com/photo/2016/03/10/18/44/top-view-1248955_1280.jpg" 
+              alt="음식" />
+          </div>
+      </Carousel>
       </div>
+        <div className="form">
+          <div>
+            <h1 className="category_title">Best</h1>
+            <p className="more">더보기</p>
+          </div>
+
+          <div>
+            <h1 className="category_title" >Sale</h1>
+            <p className="more" >더보기</p>
+          </div>
+
+          <div>
+            <h1 className="category_title" >Recipe</h1>
+            <p className="more" >더보기</p>
+          </div>
+
+          <div> 
+            <h1 className="category_title" >물물교환</h1>
+            <p className="more" >더보기</p>
+          </div>
+        </div>
         
-        <div className="form">
-          <h1 className="category_title">Best</h1>
-          <p className="more">더보기</p>
-        </div>
-
-        <div className="form">
-          <h1 className="category_title" >Sale</h1>
-          <p className="more" >더보기</p>
-        </div>
-
-        <div className="form">
-          <h1 className="category_title" >Recipe</h1>
-          <p className="more" >더보기</p>
-        </div>
-
-        <div className="form"> 
-          <h1 className="category_title" >물물교환</h1>
-          <p className="more" >더보기</p>
-        </div>
       <Footer />
       </>
     )
   }
 
 export default withRouter(MainPage)
-
