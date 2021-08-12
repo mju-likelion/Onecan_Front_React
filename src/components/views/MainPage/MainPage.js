75496163ea612bdb52e393ac543a438a56b1d70a
yuraup <o721@naver.com>
2021-08-12 20:57:44 +0900

    헤더 및 메인페이지 디자인 수정

--- src/components/views/MainPage/MainPage.js
+++ src/components/views/MainPage/MainPage.js
@@ -10,7 +10,7 @@ function MainPage(props) {
 
     const imgStyle = {
       width: '100%',
-      height: '300px',
+      height: '380px',
     };
 
     const style = { 
@@ -33,32 +33,32 @@ function MainPage(props) {
         width: '100%',
         height: '120px'
       }}>
-        <Carousel autoplay>
-          <div>
-            <img style={imgStyle}
-              src="https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466_1280.jpg" 
-              alt="음식" />
-          </div>
-          <div>
-            <img style={imgStyle}
-              src="https://cdn.pixabay.com/photo/2017/08/30/17/12/waffle-hearts-2697904_1280.jpg" 
-              alt="음식" />
-          </div>
-          <div>
-            <img style={imgStyle}
-              src="https://cdn.pixabay.com/photo/2016/11/06/23/31/breakfast-1804457_1280.jpg" 
-              alt="음식" />
-          </div>
-          <div>
-            <img style={imgStyle}
-              src="https://cdn.pixabay.com/photo/2016/03/10/18/44/top-view-1248955_1280.jpg" 
-              alt="음식" />
-          </div>
-      </Carousel>
+          <Carousel autoplay>
+            <div>
+              <img style={imgStyle}
+                src="https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466_1280.jpg" 
+                alt="음식" />
+            </div>
+            <div>
+              <img style={imgStyle}
+                src="https://cdn.pixabay.com/photo/2017/08/30/17/12/waffle-hearts-2697904_1280.jpg" 
+                alt="음식" />
+            </div>
+            <div>
+              <img style={imgStyle}
+                src="https://cdn.pixabay.com/photo/2016/11/06/23/31/breakfast-1804457_1280.jpg" 
+                alt="음식" />
+            </div>
+            <div>
+              <img style={imgStyle}
+                src="https://cdn.pixabay.com/photo/2016/03/10/18/44/top-view-1248955_1280.jpg" 
+                alt="음식" />
+            </div>
+        </Carousel>
       </div>
         <div className="form">
           <div>
-            <h1 className="categoryTitle">Best</h1>
+            <h1 className="categoryTitleFirst">Best 상품<p className="more">더보기</p></h1>
             <Row className="gutter">
               <Col className="gutter-row">
                 <img style={style}
@@ -89,11 +89,10 @@ function MainPage(props) {
                 <p className="list_price">가격</p>
               </Col>
             </Row>
-            <p className="more">더보기</p>
           </div>
 
           <div>
-            <h1 className="categoryTitle" >Sale</h1>
+            <h1 className="categoryTitle" >Sale 상품 <p className="more">더보기</p></h1>
             <div>
             <Row className="gutter">
               <Col className="gutter-row">
@@ -125,12 +124,11 @@ function MainPage(props) {
                 <p className="list_price">가격</p>
               </Col>
             </Row>
-            <p className="more">더보기</p>
           </div>
           </div>
 
           <div className="thirdBlock">
-            <h1 className="categoryTitle" >Recipe</h1>
+            <h1 className="categoryTitle" >Recipe <p className="more">더보기</p></h1>
             <div>
             <Row className="gutter">
               <Col className="gutter-row">
@@ -154,44 +152,36 @@ function MainPage(props) {
                   alt="음식" />
               </Col>
             </Row>
-            <p className="more">더보기</p>
+            <br />
           </div>
           </div>
 
           <div className="fourthBlock"> 
-            <h1 className="categoryTitle" >물물교환</h1>
+            <h1 className="categoryTitle" >물물교환 <p className="more">더보기</p></h1>
             <div>
             <Row className="gutter">
               <Col className="gutter-row">
                 <img style={style}
                   src="https://cdn.pixabay.com/photo/2015/03/26/09/45/grapes-690230_1280.jpg" 
                   alt="음식" />
-                <p className="list_name">이름</p>
-                <p className="list_price">가격</p>
               </Col>
               <Col className="gutter-row">
                 <img style={style}
                   src="https://cdn.pixabay.com/photo/2015/03/26/09/45/grapes-690230_1280.jpg" 
                   alt="음식" />
-                <p className="list_name">이름</p>
-                <p className="list_price">가격</p>
               </Col>
               <Col className="gutter-row">
                 <img style={style}
                   src="https://cdn.pixabay.com/photo/2015/03/26/09/45/grapes-690230_1280.jpg" 
                   alt="음식" />
-                <p className="list_name">이름</p>
-                <p className="list_price">가격</p>
               </Col>
               <Col className="gutter-row">
                 <img style={style}
                   src="https://cdn.pixabay.com/photo/2015/03/26/09/45/grapes-690230_1280.jpg" 
                   alt="음식" />
-                <p className="list_name">이름</p>
-                <p className="list_price">가격</p>
               </Col>
             </Row>
-            <p className="more">더보기</p>
+            <br />
           </div>
           </div>
         </div>
