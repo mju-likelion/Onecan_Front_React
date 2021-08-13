102d7a6d27967484b5dcee8bb7e7b28090247f3b
yuraup <o721@naver.com>
2021-08-13 22:19:23 +0900

    메인페이지 api

--- src/components/views/MainPage/MainPage.js
+++ src/components/views/MainPage/MainPage.js
@@ -7,7 +7,7 @@ import { Carousel, Row, Col } from 'antd';
 import './MainPage.css';
 import {RightOutlined} from '@ant-design/icons'; //아이콘 import 오류 해결
 
-function MainPage(props) {
+function MainPage() {
 
     const imgStyle = {
       width: '100%',
@@ -21,21 +21,27 @@ function MainPage(props) {
     };
 
   const [product, setProduct] = useState([]);
+  const [new_product, setNew_Product] = useState([]);
   const [recipe, setRecipe] = useState([]);
   const [barter, setBarter] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:8000/product/')
+    axios.get('http://localhost:8000/product/') //best api
       .then(response => {
         console.log(response);
         setProduct(response.data.slice(0, 4))
       })
-    axios.get('http://localhost:8000/recipe/')
+      axios.get('http://localhost:8000/new_product/') //new api
+      .then(response => {
+        console.log(response);
+        setNew_Product(response.data.slice(0, 4))
+      })
+    axios.get('http://localhost:8000/recipe/') //recipe api
       .then(response => {
         console.log(response);
         setRecipe(response.data.slice(0, 4))
       })
-    axios.get('http://localhost:8000/barter/')
+    axios.get('http://localhost:8000/barter/') //barter api
       .then(response => {
         console.log(response);
         setBarter(response.data.slice(0, 4))
@@ -50,7 +56,7 @@ function MainPage(props) {
         width: '100%',
         height: '120px'
       }}>
-          <Carousel autoplay>
+          <Carousel autoplay> 
             <div>
               <img style={imgStyle}
                 src="https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466_1280.jpg"
@@ -71,88 +77,52 @@ function MainPage(props) {
                 src="https://cdn.pixabay.com/photo/2016/03/10/18/44/top-view-1248955_1280.jpg"
                 alt="음식" />
             </div>
-        </Carousel>
+        </Carousel> 
       </div>
 
         <div className="form">
-          <div>
-            <h1 className="categoryTitleFirst">
-              <Link to='/listobj/:listId' style={{color: '#4d804a'}}>
-                Best<RightOutlined /></Link>
-            </h1>
-            <Row className="gutter">
-              <Col className="gutter-row">
-                <img style={style}
-                  src="https://cdn.pixabay.com/photo/2015/03/26/09/45/grapes-690230_1280.jpg"
-                  alt="음식" />
-                <p className="list_name">이름</p>
-                <p className="list_price">가격</p>
-              </Col>
-              <Col className="gutter-row">
-                <img style={style}
-                  src="https://cdn.pixabay.com/photo/2015/06/10/19/56/apples-805124_960_720.jpg"
-                  alt="음식" />
-                <p className="list_name">이름</p>
-                <p className="list_price">가격</p>
-              </Col>
-              <Col className="gutter-row">
-                <img style={style}
-                  src="https://cdn.pixabay.com/photo/2015/05/07/13/49/traditional-756493_960_720.jpg"
-                  alt="음식" />
-                <p className="list_name">이름</p>
-                <p className="list_price">가격</p>
-              </Col>
-              <Col className="gutter-row">
-                <img style={style}
-                  src="https://cdn.pixabay.com/photo/2017/01/20/15/06/oranges-1995056_960_720.jpg"
-                  alt="음식" />
-                <p className="list_name">이름</p>
-                <p className="list_price">가격</p>
-              </Col>
-            </Row>
-            <br />
-            <br />
-          </div>
-
-          <div>
+          <div className="firstBlock">
+              <h1 className="categoryTitleFirst">
+                <Link to='/listobj/:listId' style={{color: '#4d804a'}}>
+                  Best<RightOutlined /></Link>
+              </h1>
+              <div>
+                <Row className="gutter">
+                  {product.map(item => (
+                    <Col className="gutter-row">
+                    <img style={style}
+                      src={item.image}
+                      alt="Best 상품" />
+                    <p className="list_name">{item.prodcut}</p>
+                    <p className="list_price">{item.price}</p>
+                  </Col>
+                  ))}
+                </Row>
+                <br />
+                <br />
+              </div>
+            </div>
+          <div className="secondBlock">
               <h1 className="categoryTitle">
                 <Link to='/listobj/:listId' style={{color: '#4d804a'}}>
                   신상품<RightOutlined /></Link>
               </h1> 
             <div>
-            <Row className="gutter">
-              <Col className="gutter-row">
-                <img style={style}
-                  src="https://cdn.pixabay.com/photo/2015/03/26/09/45/grapes-690230_1280.jpg"
-                  alt="음식" />
-                <p className="list_name">이름</p>
-                <p className="list_price">가격</p>
-              </Col>
-              <Col className="gutter-row">
-                <img style={style}
-                  src="https://cdn.pixabay.com/photo/2015/03/26/09/45/grapes-690230_1280.jpg"
-                  alt="음식" />
-                <p className="list_name">이름</p>
-                <p className="list_price">가격</p>
-              </Col>
-              <Col className="gutter-row">
-                <img style={style}
-                  src="https://cdn.pixabay.com/photo/2015/03/26/09/45/grapes-690230_1280.jpg"
-                  alt="음식" />
-                <p className="list_name">이름</p>
-                <p className="list_price">가격</p>
-              </Col>
-              <Col className="gutter-row">
-                <img style={style}
-                  src="https://cdn.pixabay.com/photo/2015/03/26/09/45/grapes-690230_1280.jpg"
-                  alt="음식" />
-                <p className="list_name">이름</p>
-                <p className="list_price">가격</p>
-              </Col>
-            </Row>
-            <br />
-            <br />
-          </div>
+              <Row className="gutter">
+              
+                  {new_product.map(item => (
+                    <Col className="gutter-row">
+                        <img style={style}
+                          src={item.images}
+                          alt="음식" />
+                      <p className="list_name">{item.product}</p>
+                      <p className="list_price">{item.price}</p>
+                    </Col>
+                ))}
+              </Row>
+              <br />
+              <br />
+            </div>
           </div>
 
           <div className="thirdBlock">
@@ -160,19 +130,19 @@ function MainPage(props) {
                 <Link to='/listrecipe' style={{color: '#4d804a'}}>
                   레시피<RightOutlined /></Link>
             </h1>
-            <div>
-              <Row className="gutter">
-                {recipe.map(item => (
-                  <Col className="gutter-row">
-                    <img style={style}
-                      src={item.image}
-                      alt="음식" />
-                  </Col>
-                ))}
-              </Row>
-            <br />
-            <br />
-          </div>
+              <div>
+                <Row className="gutter">
+                  {recipe.map(item => (
+                    <Col className="gutter-row">
+                      <img style={style}
+                        src={item.image}
+                        alt="음식" />
+                    </Col>
+                  ))}
+                </Row>
+                <br />
+                <br />
+            </div>
           </div>
 
           <div className="fourthBlock">
